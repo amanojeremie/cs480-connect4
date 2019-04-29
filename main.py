76d92da8ad510b19ca01ac0a21e8c53b3f2b3bdb
Jeremie Amano <amanojeremie@gmail.com>
2019-04-29 12:07:35 -0500

    Adds credits

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+"""
+    This CS 480 project was created by Jeremie Amano, Tyler Centorbi, Minghao Shan, and Christian Thomas
+"""
 from Connect4UI import startConnect4UI
 def main():
     startConnect4UI()
